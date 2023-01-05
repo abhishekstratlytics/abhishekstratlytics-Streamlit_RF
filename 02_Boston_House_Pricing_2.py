b039d08ab0b305126c00e6e95ef5483b0ff5a8cb
abhishekstratlytics <50991993+abhishekstratlytics@users.noreply.github.com>
2023-01-05 15:01:25 +0530

    02_Boston_House_Pricing_2

--- 02_Boston_House_Pricing_2.py
+++ 02_Boston_House_Pricing_2.py
@@ -30,38 +30,38 @@ st.write(Y.head(2))
 # Sidebar
 # Header of Specify Input Parameters
 st.sidebar.header('Specify Input Parameters')
-st.sidebar.slider('CRIM',min(X.CRIM),max(X.CRIM))
-# def user_input_features():
-#     CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-#     ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-#     INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-#     CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-#     NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-#     RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-#     AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-#     DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-#     RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-#     TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-#     PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-#     B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-#     LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-#     data = {'CRIM': CRIM,
-#             'ZN': ZN,
-#             'INDUS': INDUS,
-#             'CHAS': CHAS,
-#             'NOX': NOX,
-#             'RM': RM,
-#             'AGE': AGE,
-#             'DIS': DIS,
-#             'RAD': RAD,
-#             'TAX': TAX,
-#             'PTRATIO': PTRATIO,
-#             'B': B,
-#             'LSTAT': LSTAT}
-#     features = pd.DataFrame(data, index=[0])
-#     return features
 
-# df = user_input_features()
+def user_input_features():
+    CRIM = st.sidebar.slider('CRIM', min(X.CRIM), max(X.CRIM), np.mean(X.CRIM))
+    ZN = st.sidebar.slider('ZN', min(X.ZN), max(X.ZN), np.mean(X.ZN))
+    INDUS = st.sidebar.slider('INDUS', min(X.INDUS), max(X.INDUS), np.mean(X.INDUS))
+    CHAS = st.sidebar.slider('CHAS', min(X.CHAS), max(X.CHAS), np.mean(X.CHAS))
+    NOX = st.sidebar.slider('NOX', min(X.NOX.), max(X.NOX.), np.mean(X.NOX))
+    RM = st.sidebar.slider('RM', min(X.RM), max(X.RM), np.mean(X.RM))
+    AGE = st.sidebar.slider('AGE', min(X.AGE), max(X.AGE), np.mean(X.AGE))
+    DIS = st.sidebar.slider('DIS', min(X.DIS), max(X.DIS), np.mean(X.DIS))
+    RAD = st.sidebar.slider('RAD', min(X.RAD), max(X.RAD), np.mean(X.RAD))
+    TAX = st.sidebar.slider('TAX', min(X.TAX), max(X.TAX), np.mean(X.TAX))
+    PTRATIO = st.sidebar.slider('PTRATIO', .min(X.PTRATIO), max(X.PTRATIO), np.mean(X.PTRATIO))
+    B = st.sidebar.slider('B', min(X.B), max(X.B), np.mean(X.B))
+    LSTAT = st.sidebar.slider('LSTAT', (X.LSTAT), max(X.LSTAT), np.mean(X.LSTAT))
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'RM': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'B': B,
+            'LSTAT': LSTAT}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
 
 # Main Panel
 
