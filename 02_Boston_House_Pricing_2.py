012a8a80a4ce72c8b9c5b7fd6a957f36b57eca62
abhishekstratlytics <50991993+abhishekstratlytics@users.noreply.github.com>
2023-01-05 16:21:53 +0530

    Update 02_Boston_House_Pricing_2.py

--- 02_Boston_House_Pricing_2.py
+++ 02_Boston_House_Pricing_2.py
@@ -73,7 +73,8 @@ d=d.sort_values(by='Features Score', ascending=False)
 d.reset_index(inplace=True)
 d.rename({'index':'Features'},axis=1,inplace=True)
 st.dataframe(d, width=500, height=500)
-st.bar_chart(d)
+d2=d.T
+st.bar_chart(d2)
 #st.pyplot(fig)
 # # Explaining the model's predictions using SHAP values
 # # https://github.com/slundberg/shap
