b753d790cd63b44cd2065554bf7f7722bfdad038
abhishekstratlytics <50991993+abhishekstratlytics@users.noreply.github.com>
2023-01-05 15:45:14 +0530

    02_Boston_House_Pricing_2

--- 02_Boston_House_Pricing_2.py
+++ 02_Boston_House_Pricing_2.py
@@ -44,46 +44,14 @@ B = st.sidebar.slider('B',min(X.B),max(X.B),mean(X.B))
 LSTAT = st.sidebar.slider('LSTAT',min(X.LSTAT),max(X.LSTAT),mean(X.LSTAT))
 data = {'CRIM': CRIM,'ZN': ZN,'INDUS': INDUS,'CHAS': CHAS,'NOX': NOX,'RM': RM,'AGE': AGE,'DIS': DIS,'RAD': RAD,'TAX': TAX,'PTRATIO': PTRATIO,'B': B,'LSTAT': LSTAT}
 st.write(data)
-features = pd.DataFrame(data, index=[0])
-st.write(features)
-# def user_input_features():
-#     CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-#     ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-#     INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-#     CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-#     NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-#     RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-#     AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-#     DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-#     RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-#     TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-#     PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-#     B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-#     LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-#     data = {'CRIM': CRIM,
-#             'ZN': ZN,
-#             'INDUS': INDUS,
-#             'CHAS': CHAS,
-#             'NOX': NOX,
-#             'RM': RM,
-#             'AGE': AGE,
-#             'DIS': DIS,
-#             'RAD': RAD,
-#             'TAX': TAX,
-#             'PTRATIO': PTRATIO,
-#             'B': B,
-#             'LSTAT': LSTAT}
-#     features = pd.DataFrame(data, index=[0])
-#     return features
-# st.write(features)
-# #df = user_input_features()
+df = pd.DataFrame(data, index=[0])
 
 # Main Panel
 
 # # Print specified input parameters
-# st.header('Specified Input parameters')
-# st.write(df)
-# st.write('---')
+st.header('Specified Input parameters')
+st.write(df)
+st.write('---')
 
 # # Build Regression Model
 # model = RandomForestRegressor()
